89cd11f38f9959bfe349def7d672d14dec5200f4
Juan <saraviamartin16@hotmail.com>
2023-09-26 19:09:17 -0300

    createNewUser component

--- src/store/users/slice.ts
+++ src/store/users/slice.ts
@@ -51,6 +51,10 @@ export const usersSlice = createSlice({
 	name: "users",
 	initialState,
 	reducers: {
+		addNewUser: (state, action: PayloadAction<User>) => {
+			const id = crypto.randomUUID();
+			return [...state, { id, ...action.payload }];
+		},
 		deleteUserById: (state, action: PayloadAction<UserId>) => {
 			const id = action.payload;
 			return state.filter((user) => user.id !== id);
@@ -60,4 +64,4 @@ export const usersSlice = createSlice({
 
 export default usersSlice.reducer;
 
-export const { deleteUserById } = usersSlice.actions;
+export const { addNewUser, deleteUserById } = usersSlice.actions;
