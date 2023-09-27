c83e26ed421e5f77b6a8ba96e2a2e27d8e4f143f
Juan <saraviamartin16@hotmail.com>
2023-09-27 17:50:51 -0300

    rollback borrando usuario

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,51 @@
-import { configureStore } from "@reduxjs/toolkit";
-import usersReducer from "./users/slice";
+import { configureStore, type Middleware } from "@reduxjs/toolkit";
+import { toast } from "sonner";
+import usersReducer, { rollbackUser } from "./users/slice";
 
-const persistanceLocalStorageMiddleware = (store) => (next) => (action) => {
-	next(action);
-	localStorage.setItem("__redux__state__", JSON.stringify(store.getState()));
-};
+const persistanceLocalStorageMiddleware: Middleware =
+	(store) => (next) => (action) => {
+		next(action);
+		localStorage.setItem("__redux__state__", JSON.stringify(store.getState()));
+	};
+
+const syncWithDataBaseMiddleware: Middleware =
+	(store) => (next) => (action) => {
+		const { type, payload } = action;
+		const previousState = store.getState();
+		next(action);
+
+		if (type === "users/deleteUserById") {
+			const userIdToRemove = payload;
+			//Eliminando usuario
+			const userToRemove = previousState.users.find(
+				(user) => user.id === userIdToRemove,
+			);
+
+			fetch(`https://jsonplaceholder.typicode.com/users/${userIdToRemove}`, {
+				method: "DELETE",
+			})
+				.then((res) => {
+					if (res.ok) {
+						toast.success(`Usuario ${userIdToRemove} eliminado correctamente`);
+					}
+					throw new Error("Error al eliminar el usuario");
+				})
+				.catch((err) => {
+					toast.error(
+						`El usuario ${userIdToRemove} no se pudo eliminar correctamente`,
+					);
+					if (userToRemove) store.dispatch(rollbackUser(userToRemove));
+					console.log("error");
+					console.log(err);
+				});
+		}
+	};
 
 export const store = configureStore({
 	reducer: {
 		users: usersReducer,
 	},
-	middleware: [persistanceLocalStorageMiddleware],
+	middleware: [persistanceLocalStorageMiddleware, syncWithDataBaseMiddleware],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
